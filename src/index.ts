6fd72a25d14f0a61b1e5d0ed24bc85bdc0741413
Bruno Coronado <brunocoronado@Braunybrunista666.local>
2022-12-10 22:01:42 -0600

    Curso terminado

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,6 @@ console.log(mensaje);
 
 mensaje = "Adios mundo";
 
-
 let extincionDino: number = 75_5455_55;
 let extintos: boolean = true;
 let dinoFav: string = "T-rex";
@@ -19,34 +18,49 @@ function chanchoFeliz(config: string) {
 console.log(chanchoFeliz("Bruce"));
 
 let animales: string[] = ["Perro", "Gato", "Ave"];
-let numeros: number[] = [1,2,3,4,5];
+let numeros: number[] = [1, 2, 3, 4, 5];
 let facts: boolean[] = [true, false, false];
 
 // Manera diferente de declarar un arreglo
-let nums2: Array<number> = []
+let nums2: Array<number> = [];
 
-const animalesUpper = animales.map(x => x.toUpperCase());
-const numerosString = numeros.map(y => y.toString());
+const animalesUpper = animales.map((x) => x.toUpperCase());
+const numerosString = numeros.map((y) => y.toString());
 
 console.log(animalesUpper);
 console.log(numerosString);
 
-let tupla: [number, string] = [1, 'Perro'];
-let tupla2: [number, string[]] = [1, ['Perro', 'Gato']];
+let tupla: [number, string] = [1, "Perro"];
+let tupla2: [number, string[]] = [1, ["Perro", "Gato"]];
 
 // Sin enum
-const chica = 's';
-const mediana = 'm';
-const grande = 'l';
-const extraGrande = 'xl';
+const chica = "s";
+const mediana = "m";
+const grande = "l";
+const extraGrande = "xl";
 
 // Con enum, PascalCase
 enum Talla {
-    Chica = 's', // => 0
-    Mediana = 'm', // => 1
-    Grande = 'l', // => 2
-    Extragrande = 'xl' // => 3
+    Chica = "s", // => 0
+    Mediana = "m", // => 1
+    Grande = "l", // => 2
+    Extragrande = "xl", // => 3
 }
 
 const var1 = Talla.Grande;
 console.log(`Talla: ${var1}`);
+
+const objeto = { id: 1, nombre: "" };
+objeto.nombre = "Bruce";
+
+type Persona = {
+    readonly id: number;
+    nombre?: string;
+    talla: Talla
+};
+
+const objeto2: Persona = {
+    id: 1,
+    nombre: "Bruce",
+    talla: Talla.Chica,
+};
